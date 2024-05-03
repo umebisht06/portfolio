028cef613d15cd96d4ec7a47557b088bd69cbdc6
umebisht06 <162161282+umebisht06@users.noreply.github.com>
2024-05-03 15:57:33 +0530

    Add files via upload

---
+++ Heading.js
@@ -0,0 +1,10 @@
+
+      let type = new Typed("#element", {
+        strings: [
+        "Web Developer.",
+        "Graphic Designer.",
+        "Web Designer",
+        "Video Editor.",
+        ],
+        typeSpeed: 50,
+      });
